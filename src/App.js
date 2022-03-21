3d950cc8d9cfc8315fbe1e3d234eba83354d44bc
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-03-22 01:52:54 +0600

    header section is working

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Header from "./Components/Header/Header";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <Header></Header>
+    </div>
+  );
 }
 
 export default App;
