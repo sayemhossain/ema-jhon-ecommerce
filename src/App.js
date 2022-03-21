6360861bfd46da886d9a8cb7439d8e589e102da0
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-03-22 04:23:24 +0600

    cart is processing

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Shop from "./Components/Shop/Shop";
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <Header></Header>
       <Shop></Shop>
     </div>
--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -2,14 +2,32 @@ import React from "react";
 import "./Product.css";
 
 const Product = (props) => {
-  console.log(props);
+  //   console.log(props);
   const { img, name, seller, price, ratings } = props.product;
 
   return (
     <div className="product">
-      <img src={img} alt="" />
-      <h2>{name}</h2>
-      <p>Price: {price}</p>
+      <div className="img-container">
+        <img src={img} alt="" />
+      </div>
+      <div className="product-details">
+        <h2>{name}</h2>
+        <p className="price">Price: ${price}</p>
+        <div className="common">
+          <p>
+            <small>Manufacturer: {seller}</small>{" "}
+          </p>
+          <p>
+            <small>Rating: {ratings} start</small>
+          </p>
+        </div>
+      </div>
+      <div
+        className="btn-cart"
+        onClick={() => props.handleAddToCart(props.Product)}
+      >
+        Add to Cart
+      </div>
     </div>
   );
 };
