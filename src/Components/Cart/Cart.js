3d9cb57b34071e86c54c2c47d08df5522137ed19
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-03-23 01:37:29 +0600

    cart calculation done

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -8,24 +8,22 @@ const Cart = (props) => {
 
   //   this is for adding total price
   let total = 0;
+  let shippingCost = 0;
   for (const product of cart) {
     total = total + product.price;
+    shippingCost += product.shipping;
   }
-  //this is for shpping price
-  let totalPriceForShipping = 0;
-  for (const product of cart) {
-    totalPriceForShipping += product.shipping;
-  }
+
   //   this is for tax
   const tax = (total * 5) / 100;
   //   this is for grand total
-  const grandTotal = total + tax + totalPriceForShipping;
+  const grandTotal = total + tax + shippingCost;
 
   return (
     <div>
       <p>Selected Items: {cart.length}</p>
       <p>Total Price: ${total}</p>
-      <p>Total Shipping Charge: ${totalPriceForShipping}</p>
+      <p>Total Shipping Charge: ${shippingCost}</p>
       <p>Tax: ${tax}</p>
       <h4>Grand Total: ${grandTotal}</h4>
       <div className="btn btn-one">
