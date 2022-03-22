6c15abab3ed7dbff709579dc10c9f57292ed2577
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-03-23 01:32:48 +0600

    cart calculation done

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -4,13 +4,30 @@ import { faTrashCan, faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import "./Cart.css";
 
 const Cart = (props) => {
+  const { cart } = props;
+
+  //   this is for adding total price
+  let total = 0;
+  for (const product of cart) {
+    total = total + product.price;
+  }
+  //this is for shpping price
+  let totalPriceForShipping = 0;
+  for (const product of cart) {
+    totalPriceForShipping += product.shipping;
+  }
+  //   this is for tax
+  const tax = (total * 5) / 100;
+  //   this is for grand total
+  const grandTotal = total + tax + totalPriceForShipping;
+
   return (
     <div>
-      <p>Selected Items: {props.cart.length}</p>
-      <p>Total Price: ${props.cart.length}</p>
-      <p>Total Shipping Charge: ${props.cart.length}</p>
-      <p>Tax: ${props.cart.length}</p>
-      <h4>Grand Total: ${props.cart.length}</h4>
+      <p>Selected Items: {cart.length}</p>
+      <p>Total Price: ${total}</p>
+      <p>Total Shipping Charge: ${totalPriceForShipping}</p>
+      <p>Tax: ${tax}</p>
+      <h4>Grand Total: ${grandTotal}</h4>
       <div className="btn btn-one">
         Clear Cart <FontAwesomeIcon icon={faTrashCan} />
       </div>
