5560a20cd05116d071257446f5b959123c0cf22a
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-03-24 23:59:45 +0600

    new code

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -19,7 +19,7 @@ const Cart = (props) => {
 
   return (
     <div>
-      <p>Selected Items: {cart.length} </p>
+      <p>Selected Items: {quantity} </p>
       <p>Total Price: ${total}</p>
       <p>Total Shipping Charge: ${shippingCost}</p>
       <p>Tax: ${tax}</p>
